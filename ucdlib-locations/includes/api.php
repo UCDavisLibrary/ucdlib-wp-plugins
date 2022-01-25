c0a0344c96ce10a0514534bc3ae875a7e5578f4e
spelkey-ucd <spelkey@ucdavis.edu>
2022-01-25 16:30:51 -0500

    #4

--- ucdlib-locations/includes/api.php
+++ ucdlib-locations/includes/api.php
@@ -18,14 +18,24 @@ class UCDLibPluginLocationsAPI {
         'fields' => self::$location_fields
       ]
     ) );
+
+    register_rest_route($this->config['slug'], 'locations/(?P<id>\d+)', array(
+      'methods' => 'GET',
+      'callback' => array($this, 'epcb_location'),
+      'permission_callback' => function (){return true;},
+      'args' => [
+        'fields' => self::$location_fields
+      ]
+    ) );
   }
 
   public function epcb_locations($request){
     $fields = $request['fields'];
-    //return rest_ensure_response("hi there");
 
     $locations = Timber::get_posts( [
       'post_type' => $this->config['postTypeSlug'],
+      'orderby' => 'menu_order',
+      'order' => 'ASC',
       'nopaging' => true,
     ] );
 
@@ -33,23 +43,46 @@ class UCDLibPluginLocationsAPI {
     $out = array();
     foreach ($locations as $location) {
       $loc = $location->core_data();
-
-
-      if ( in_array('hours', $fields) ) {
-        $loc['hours'] = $location->get_hours();
-      } elseif ( in_array('hours-today', $fields) ) {
-        $hours = $location->get_hours();
-        $loc['hoursToday'] = UCDLibPluginLocationsUtils::getTodaysHours($hours);
-      }
-
-      if ( in_array('occupancy-now', $fields) ) {
-        $loc['occupancyNow'] = $location->get_current_occupancy();
-      }
-
+      $loc = array_merge($loc, $this->add_additional_fields($location, $fields) );
       
       $out[] = $loc;
     }
     return rest_ensure_response($out);
+  }
+
+  public function epcb_location($request){
+    $fields = $request['fields'];
+    $locationId = $request['id'];
+
+    $location = Timber::get_posts( [
+      'post_type' => $this->config['postTypeSlug'],
+      'p' => $locationId
+    ] );
+    if ( $location->found_posts ) {
+      $location = $location[0];
+      $out = $location->core_data();
+      $out = array_merge($out, $this->add_additional_fields($location, $fields) );
+      return rest_ensure_response($out);
+    } 
+
+    return new WP_Error( 'rest_not_found', 'This location does not exist.', array( 'status' => 404 ) );
+
+  }
+
+  private function add_additional_fields($location, $fields){
+    $out = array();
+    if ( in_array('hours', $fields) ) {
+      $out['hours'] = $location->get_hours();
+    } elseif ( in_array('hours-today', $fields) ) {
+      $hours = $location->get_hours();
+      $out['hoursToday'] = UCDLibPluginLocationsUtils::getTodaysHours($hours);
+    }
+
+    if ( in_array('occupancy-now', $fields) ) {
+      $out['occupancyNow'] = $location->get_current_occupancy();
+    }
+
+    return $out;
   }
 
   public static $location_fields = array(
--- ucdlib-locations/includes/location.php
+++ ucdlib-locations/includes/location.php
@@ -13,7 +13,8 @@ class UCDLibPluginLocationsLocation extends Timber\Post {
       'labels' => $this->meta('labels'),
       'links' => array(),
       'parent' => $this->meta('parent'),
-      'appointments' => $this->meta('appointments')
+      'appointments' => $this->meta('appointments'),
+      'order' => $this->menu_order
     );
 
     $out['labels']['title'] = $this->title();
@@ -28,6 +29,12 @@ class UCDLibPluginLocationsLocation extends Timber\Post {
       $out['appointments'] = array(
         'required' => false
       );
+    }
+    $capacity = $this->meta('capacity');
+    if ( $capacity ) $out['capacity'] = intval($capacity);
+
+    if ( $out['parent'] ){
+      $out['notACollapsibleChild'] = $this->meta('not_collapsible');
     }
 
     $this->core_data = $out;
@@ -47,12 +54,12 @@ class UCDLibPluginLocationsLocation extends Timber\Post {
       $out[] = "Missing Safespace credentials in settings page";
     }
 
-    elseif ( $this->meta('show_current_occupancy') ) {
-      //$out[0] = true;
+    elseif ( $this->meta('safespace_id') ) {
+      return array(true);
     }
 
     else {
-      $out[] = "No current occupancy data for this location.";
+      $out[] = "Location does not have a safespace id";
     }
 
     $this->can_get_current_occupancy = $out;
@@ -68,23 +75,69 @@ class UCDLibPluginLocationsLocation extends Timber\Post {
       return $this->get_current_occupancy;
     }
 
+    $this->get_current_occupancy = $this->get_safespace_occupancy();
+    return $this->get_current_occupancy;
+  }
+
+  private function get_safespace_occupancy(){
     $out = array(
       'status' => 'error',
-      'data' => false
+      'data' => false,
+      'source' => 'safespace'
     );
 
-    // check if this location can get current occupancy
-    $can_get_current_occupancy = $this->can_get_current_occupancy();
-    if ( !$can_get_current_occupancy[0] ){
-      if ( isset($can_get_current_occupancy[1]) ) {
-        $out['message'] = $can_get_current_occupancy[1];
+    // if current occupancy is not enabled on location page, bail
+    if ( !$this->meta('show_current_occupancy') ){
+      $out['status'] = 'good';
+      $out['message'] = "Location does not have public current capacity data";
+      return $out;
+    }
+  
+    // check cache
+    $transient_id = 'safespace_occupancy_' . $this->ID;
+    $t = get_transient( $transient_id );
+
+    // libcal api call has been made before, and is cached
+    if ( $t !== false ) {
+      $out['status'] = $t['status'];
+      $out['cached'] = $t['cached'];
+      $out['data'] = $t['data'];
+      if ( array_key_exists('message', $t) ){
+        $out['message'] = $t['message'];
       }
-      $this->get_current_occupancy = $out;
-      return $this->get_current_occupancy;
+
+    // api call not cached. need to perform.
+    } else {
+
+      // check if this location is set up to get occupancy
+      $can_get_current_occupancy = $this->can_get_current_occupancy();
+      if ( !$can_get_current_occupancy[0] ){
+        if ( isset($can_get_current_occupancy[1]) ) {
+          $out['message'] = $can_get_current_occupancy[1];
+        }
+        return $out;
+      }
+
+      # construct GET request for libcal location
+      $creds = get_field('api_sensource_safespace', $this->post_type);
+      $url = trailingslashit($creds['url']) . $this->meta('safespace_id');
+      $out['cached'] = time();
+
+      // do GET request and cache data
+      $r = wp_remote_get($url);
+      if (wp_remote_retrieve_response_code($r) == 200) {
+        $data = json_decode(wp_remote_retrieve_body($r), true);
+        $out['status'] = 'good';
+        $out['data'] = $data;
+        set_transient( $transient_id, $out, 180 );
+      }
+      else {
+        $out['message'] = 'Safespace API is unavailable.';
+        set_transient( $transient_id, $out, 30 );
+      }
     }
 
-    $this->get_current_occupancy = $out;
-    return $this->get_current_occupancy;
+    return $out;
   }
 
   // Checks if location is properly set up to retrieve hours
@@ -137,106 +190,113 @@ class UCDLibPluginLocationsLocation extends Timber\Post {
     );
 
     if ( $this->meta('hours_system') === 'libcal' ){
-
-      // if hours are not enabled on location admin page, bail.
-      if ( !$this->meta('has_operating_hours') ){
-        $out['status'] = 'good';
-        $out['message'] = "Location does not have public operating hours.";
-        $this->get_hours = $out;
-        return $this->get_hours;
-      }
-
-      // check cache
-      $transient_id = 'libcal_hours_' . $this->ID;
-      $t = get_transient( $transient_id );
-
-      // libcal api call has been made before, and is cached
-      if ( $t !== false ) {
-        $out = array(
-          'status' => $t['status'],
-          'cached' => $t['cached']
-        );
-        if ( 
-          array_key_exists('data', $t) && 
-          array_key_exists('dates', $t['data'])
-        ){
-          $out['data'] = $t['data']['dates'];
-        } else {
-          $out['data'] = false;
-        }
-        if ( array_key_exists('message', $t) ){
-          $out['message'] = $t['message'];
-        }
-
-      // libcal api call not cached. need to perform.
-      } else {
-
-        // check if this location can get hours
-        $can_get_hours = $this->can_get_hours();
-        if ( !$can_get_hours[0] ){
-          if ( isset($can_get_hours[1]) ) {
-            $out['message'] = $can_get_hours[1];
-          }
-          $this->get_hours = $out;
-          return $this->get_hours;
-        }
-        $creds = get_field('api_libcal', $this->post_type);
-      
-        // get access token
-        $token = $this->get_libcal_token();
-        if ( !$token ) {
-          $out['message'] = 'Unable to get libcal API token';
-          $this->get_hours = $out;
-          return $this->get_hours;
-        }
   
-        # construct GET request for libcal location
-        $url = trailingslashit($creds['url_hours']) . $this->meta('libcal_id');
-        $dateRange = UCDLibPluginLocationsUtils::getHoursDateRange();
-        $url_params = array(
-          'from' => $dateRange[0],
-          'to' => $dateRange[1]
-        );
-        $url .= '?' . http_build_query($url_params);
-        $headers = array(
-          "Content-type" => 'application/json',
-          "Authorization" => "BEARER " . $token
-        );
-        $args = array(
-          'body'        => null,
-          'timeout'     => '10',
-          'redirection' => '5',
-          'blocking'    => true,
-          'headers'     => $headers,
-          'cookies'     => array()
-        );
-        // do GET request and cache data
-        $r = wp_remote_get($url, $args);
-        if (wp_remote_retrieve_response_code($r) == 200) {
-          $data = json_decode(wp_remote_retrieve_body($r), true);
-          $cached_data = array(
-            'status' => 'good',
-            'cached' => time(),
-            'data' => $data[0]
-          );
-          $transient_duration = get_field('hours_cache_duration', $this->post_type);
-          if ( !$transient_duration ) $transient_duration = 300;
-          set_transient( $transient_id, $cached_data, $transient_duration );
-          $out = $cached_data;
-          $out['data'] = $out['data']['dates'];
-        }
-        else {
-          $out['message'] = 'Libcal API is unavailable.';
-          $out['cached'] = time();
-          set_transient( $transient_id, $out, 30 );
-        }
-      }
-      $out['source'] = 'libcal';
-  
+      $out = $this->get_libcal_hours();
+    } else {
+      $out['message'] = "Operating hours not stored in a recognized system.";
     }
 
     $this->get_hours = $out;
     return $this->get_hours;
+  }
+
+  // Retrieves operating hours from springshare libcal
+  private function get_libcal_hours(){
+    $out = array(
+      'status' => 'error',
+      'data' => false,
+      'source' => 'libcal'
+    );
+
+    // if hours are not enabled on location admin page, bail.
+    if ( !$this->meta('has_operating_hours') ){
+      $out['status'] = 'good';
+      $out['message'] = "Location does not have public operating hours.";
+      return $out;
+    }
+
+    // check cache
+    $transient_id = 'libcal_hours_' . $this->ID;
+    $t = get_transient( $transient_id );
+
+    // libcal api call has been made before, and is cached
+    if ( $t !== false ) {
+      $out['status'] = $t['status'];
+      $out['cached'] = $t['cached'];
+      if ( 
+        array_key_exists('data', $t) && 
+        array_key_exists('dates', $t['data'])
+      ){
+        $out['data'] = $t['data']['dates'];
+      } else {
+        $out['data'] = false;
+      }
+      if ( array_key_exists('message', $t) ){
+        $out['message'] = $t['message'];
+      }
+
+    // libcal api call not cached. need to perform.
+    } else {
+
+      // check if this location can get hours
+      $can_get_hours = $this->can_get_hours();
+      if ( !$can_get_hours[0] ){
+        if ( isset($can_get_hours[1]) ) {
+          $out['message'] = $can_get_hours[1];
+        }
+        return $out;
+      }
+      $creds = get_field('api_libcal', $this->post_type);
+    
+      // get access token
+      $token = $this->get_libcal_token();
+      if ( !$token ) {
+        $out['message'] = 'Unable to get libcal API token';
+        return $out;
+      }
+
+      # construct GET request for libcal location
+      $url = trailingslashit($creds['url_hours']) . $this->meta('libcal_id');
+      $dateRange = UCDLibPluginLocationsUtils::getHoursDateRange();
+      $url_params = array(
+        'from' => $dateRange[0],
+        'to' => $dateRange[1]
+      );
+      $url .= '?' . http_build_query($url_params);
+      $headers = array(
+        "Content-type" => 'application/json',
+        "Authorization" => "BEARER " . $token
+      );
+      $args = array(
+        'body'        => null,
+        'timeout'     => '10',
+        'redirection' => '5',
+        'blocking'    => true,
+        'headers'     => $headers,
+        'cookies'     => array()
+      );
+      // do GET request and cache data
+      $r = wp_remote_get($url, $args);
+      if (wp_remote_retrieve_response_code($r) == 200) {
+        $data = json_decode(wp_remote_retrieve_body($r), true);
+        $cached_data = array(
+          'status' => 'good',
+          'cached' => time(),
+          'data' => $data[0]
+        );
+        $transient_duration = get_field('hours_cache_duration', $this->post_type);
+        if ( !$transient_duration ) $transient_duration = 300;
+        set_transient( $transient_id, $cached_data, $transient_duration );
+        $out = array_merge($out, $cached_data);
+        $out['data'] = $out['data']['dates'];
+      }
+      else {
+        $out['message'] = 'Libcal API is unavailable.';
+        $out['cached'] = time();
+        set_transient( $transient_id, $out, 30 );
+      }
+    }
+    return $out;
   }
 
 
