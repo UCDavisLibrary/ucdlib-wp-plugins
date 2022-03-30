8beda0c3f7bf923ee56146a79d4f1169594a2c07
spelkey-ucd <spelkey@ucdavis.edu>
2022-03-30 15:23:38 -0400

    #7

---
+++ ucdlib-search/includes/document.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * Wrapper class for an elasticsearch document "hit"
+ */
+class UCDLibPluginSearchDocument {
+  public function __construct($document){
+    $this->document = $document;
+    if ( in_array( $document['_source']['type'], get_post_types() ) ){
+      $this->post = Timber::get_post($document['_source']['id']);
+    } else {
+      $this->post = null;
+    }
+  }
+
+  public function title(){
+    return $this->document['_source']['title'];
+  }
+
+  public function link(){
+    if ( $this->post ){
+      return $this->post->link();
+    }
+    return $this->document['_source']['id'];
+  }
+
+  public function excerpt(){
+    if ( $this->post ){
+      return $this->post->excerpt(['read_more' => '' ]);
+    }
+    return $this->document['_source']['description'];
+  }
+
+  protected $showAuthor;
+  public function showAuthor(){
+    if ( ! empty( $this->showAuthor ) ) {
+      return $this->showAuthor;
+    }
+
+    if ( 
+      $this->author() &&
+      $this->post && 
+      $this->post->post_type == 'post' &&
+      !$this->post->meta('ucd_hide_author')
+      ) {
+        $this->showAuthor = true;
+      } else {
+        $this->showAuthor = false;
+      }
+    return $this->showAuthor;
+  }
+
+  public function showPublishedDate(){
+    if ( $this->post && $this->post->post_type == 'post' ){
+      return true;
+    }
+    return false;
+  }
+
+  public function date(){
+    if ( $this->post ) return $this->post->date();
+    return "";
+  }
+
+  protected $author;
+  public function author(){
+    if ( ! empty( $this->author ) ) {
+      return $this->author;
+    }
+    if ( $this->post && $this->post->author() ){
+      $this->author = $this->post->author();
+    } else {
+      $this->author = false;
+    }
+    return $this->author;
+  }
+}
--- ucdlib-search/includes/elasticsearch.php
+++ ucdlib-search/includes/elasticsearch.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once( __DIR__ . '/document.php' );
+
+/**
+ * Class for hijacking the main wp search, and returning results from elasticsearch.
+ */
 class UCDLibPluginSearchElasticsearch {
   public function __construct( $config, $doHooks=true ){
     $this->config = $config;
@@ -10,8 +15,6 @@ class UCDLibPluginSearchElasticsearch {
       add_filter( 'ucd-theme/context/search', array($this, 'setContext') );
       add_filter( 'ucd-theme/templates/search', array($this, 'setTemplate'), 10, 2 );
     }
-
-
   }
 
   public function createClient(){
@@ -22,16 +25,40 @@ class UCDLibPluginSearchElasticsearch {
 
   public function interceptWpSearch( $query ){
     if ( ! is_admin() && $query->is_main_query() && is_search() ) {
+
+      // save original search status for ES query
+      $this->currentPage = get_query_var('paged') ? get_query_var('paged') : 1;
+      $this->pageSize = get_option('posts_per_page');
+      $this->searchQuery = get_search_query();
       
-      // stop wp from throwing a 404 when page number is beyond what is in mysql
+      // stop wp from throwing a 404 when no posts are returned
       $query->set( 'posts_per_page', 1 );
       $query->set( 'paged', 1);
+      $query->set( 's', '' );
     }
   }
 
   public function setContext($context){
+    $context['currentPage'] = $this->currentPage;
+    $context['pageSize'] = $this->pageSize;
+    $context['search_query'] = $this->searchQuery;
+    $context['title'] = 'Search results for ' . $this->searchQuery;
     try {
       $results = $this->doMainSearch();
+      $context['results'] = array_map(function($x){return new UCDLibPluginSearchDocument($x);}, $results['hits']['hits']);
+      $context['found_posts'] = $results['hits']['total']['value'];
+      $context['hasPages'] = $context['found_posts'] > $context['pageSize'];
+      $context['lastPage'] = ceil($context['found_posts'] / $this->pageSize);
+      $context['is404'] =  $context['hasPages'] && $this->currentPage > $context['lastPage'];
+      if ( $context['hasPages'] ){
+        global $wp;  
+        $current_url = home_url(add_query_arg(array($_GET), $wp->request));
+        $current_url = preg_replace( '/page\/\d+/', '', $current_url );
+        $current_url = parse_url($current_url);
+        $context['paginationUrl'] =  $current_url;
+      }
+      return $context;
+
     } catch (\Throwable $th) {
       if ( WP_DEBUG === true ) {
         throw $th;
@@ -40,28 +67,33 @@ class UCDLibPluginSearchElasticsearch {
       }
       return $context;
     }
-    
-    $context['results'] = $results;
-    $context['found_posts'] = $results['hits']['total']['value'];
-    return $context;
+
   }
 
   public function setTemplate( $templates, $context ){
-    $templates = array_merge( array("@" . $this->config['slug'] . "/search.twig"), $templates );
+    if ( array_key_exists('is404', $context) && $context['is404']){
+      status_header(404);
+      $views = $GLOBALS['UcdSite']->views;
+      $templates = array( $views->getTemplate('404') );
+    } else {
+      $templates = array_merge( array("@" . $this->config['slug'] . "/search.twig"), $templates );
+    }
 
     return $templates;
   }
 
   public function doMainSearch(){
-    $s = get_search_query();
     $params = [
       'index' => $this->config['elasticsearch']['indexAlias'],
       'body'  => [
-          'query' => [
-              'match' => [
-                  'content' => $s
-              ]
+        'size' => $this->pageSize,
+        'from' => ($this->currentPage - 1) * $this->pageSize,
+        'query' => [
+          'multi_match' => [
+            'query' => $this->searchQuery,
+            'fields' => ['content', 'title^3']
           ]
+        ]
       ]
     ];
     $response = $this->client->search($params);
