8824670bf2a1f439ecaa43d1865a3737b2a8408c
spelkey-ucd <spelkey@ucdavis.edu>
2022-05-26 16:48:07 -0400

    tweaks to #23

--- ucdlib-search/src/public/lib/elements/ucdlib-search-filter.tpl.js
+++ ucdlib-search/src/public/lib/elements/ucdlib-search-filter.tpl.js
@@ -17,8 +17,14 @@ export function styles() {
     .light-blue {
       background-color: #ebf3fa;
     }
+    .main {
+      padding: 2rem;
+    }
     .checkbox label {
       color: #022851;
+    }
+    .checkbox label::before {
+      background-color: white;
     }
     .section-header {
       display: flex;
@@ -39,10 +45,11 @@ export function styles() {
       font-weight: inherit;
     }
     select {
-      background-color: #b0d0ed;
+      background-color: #B0D0ED;
       color: #022851;
       font-weight: 700;
       border: none;
+      font-size: 1rem;
     }
     select:focus {
       background-color: #b0d0ed;
@@ -94,7 +101,7 @@ return html`
     aria-expanded=${this.showOnMobile ? 'true' : 'false'}>
     <h2 class="panel__title">Filter Results</h2>
   </div>
-  <div class="o-box light-blue main ${this.showOnMobile ? 'show': 'hide'}">
+  <div class="light-blue main ${this.showOnMobile ? 'show': 'hide'}">
     <form @submit="${this._onSubmit}">
       <h5 class="section-header">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M3.853 54.87C10.47 40.9 24.54 32 40 32H472C487.5 32 501.5 40.9 508.1 54.87C514.8 68.84 512.7 85.37 502.1 97.33L320 320.9V448C320 460.1 313.2 471.2 302.3 476.6C291.5 482 278.5 480.9 268.8 473.6L204.8 425.6C196.7 419.6 192 410.1 192 400V320.9L9.042 97.33C-.745 85.37-2.765 68.84 3.854 54.87L3.853 54.87z"/></svg>
