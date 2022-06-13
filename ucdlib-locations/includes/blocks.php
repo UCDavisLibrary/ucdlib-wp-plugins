152f69ad0761fc23c6354d5010702760ab71f277
spelkey-ucd <spelkey@ucdavis.edu>
2022-06-13 12:05:35 -0400

    inherit from block parent class in theme

--- ucdlib-locations/includes/blocks.php
+++ ucdlib-locations/includes/blocks.php
@@ -1,12 +1,13 @@
 <?php
 require_once( __DIR__ . '/block-transformations.php' );
+require_once( get_template_directory() . '/includes/classes/block-renderer.php' );
 
 // Set up server-side rendering for gutenberg location blocks
-class UCDLibPluginLocationsBlocks {
+class UCDLibPluginLocationsBlocks extends UCDThemeBlockRenderer {
   public function __construct($config){
+    parent::__construct();
     $this->config = $config;
     $this->slug = $config['slug'];
-    $this->iconsUsed = [];
 
     $this->registry = [
       "$this->slug/hours-today" => [
@@ -20,20 +21,13 @@ class UCDLibPluginLocationsBlocks {
     ];
 
     add_action('block_categories_all', array($this, 'addCategories'), 10,2);
-    add_action( 'init', array( $this, 'register_blocks'));
-    add_filter( 'ucd-theme/loaded-icons', array($this, 'loadIcons'), 10, 1);
     
   }
 
+  public static $transformationClass = 'UCDLibPluginLocationsBlockTransformations';
+
   public function twigPath($block){
     return '@' . $this->config['slug'] . "/blocks/$block.twig";
-  }
-
-  public function loadIcons($icons){
-    foreach ($this->iconsUsed as $icon) {
-      if ( !array_key_exists($icon, $icons) ) $icons[] = $icon;
-    }
-    return $icons;
   }
 
   /**
@@ -51,72 +45,5 @@ class UCDLibPluginLocationsBlocks {
 
     return array_merge($block_categories, $customCategories);
   }
-
-  /**
-   * Registers serverside rendering callback of all plugin blocks
-   */
-  public function register_blocks( ) {
-    foreach ($this->registry as $name => $block) {
-      $settings = array(
-        'api_version' => 2, 
-        'render_callback' => array($this, 'render_callback')
-      );
-      if ( array_key_exists('uses_context', $block) ) {
-        $settings['uses_context'] = $block['uses_context'];
-      }
-      if ( array_key_exists('provides_context', $block) ) {
-        $settings['provides_context'] = $block['provides_context'];
-      };
-      register_block_type(
-        $name, 
-        $settings
-      );
-    }
-  }
-
-  /**
-   * Renders designated Twig and applies attribute transformations for a registered block
-   *  
-   */
-  public function render_callback($block_attributes, $content, $block) {
-
-    // Retrieve metadata from registry
-    $blockName = $block->name;
-    if ( !$blockName ) return;
-    $meta = $this->registry[$blockName];
-
-    // Apply any transformations to block attributes specified in registry
-    if ( array_key_exists("transform", $meta) ){
-      if ( is_array($meta['transform']) ) {
-        $transformations = $meta['transform'];
-      }
-      else {
-        $transformations = array($meta['transform']);
-      }
-      foreach ($transformations as $transformation) {
-        $block_attributes = call_user_func("UCDLibPluginLocationsBlockTransformations::" . $transformation, $block_attributes);
-      }
-    }
-
-    // check for icons (so we can only load the svgs we actually use)
-    if ( 
-      array_key_exists('icon', $block_attributes) && 
-      !in_array($block_attributes['icon'], $this->iconsUsed)) {
-        $this->iconsUsed[] = $block_attributes['icon'];
-      }
-    if ( array_key_exists('icons', $block_attributes) && is_array($block_attributes['icons'])) {
-      foreach ($block_attributes['icons'] as $icon) {
-        if ( !in_array($icon, $this->iconsUsed)) {
-          $this->iconsUsed[] = $icon;
-        }
-      }
-    }
-
-    // Render twig
-    ob_start();
-    Timber::render( $meta['twig'], array("attributes" => $block_attributes, "content" => $content, "block" => $block) );
-    return ob_get_clean();
-  }
-
 
 }
