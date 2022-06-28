df0e831940f334bb015912d9a88f67f0f8bb58f4
spelkey-ucd <spelkey@ucdavis.edu>
2022-06-28 12:00:36 -0400

    54

--- ucdlib-special/src/editor/lib/plugins/exhibit.js
+++ ucdlib-special/src/editor/lib/plugins/exhibit.js
@@ -2,6 +2,9 @@ import { Fragment } from "@wordpress/element";
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
 import { useDispatch } from "@wordpress/data";
 import { 
+  CheckboxControl,
+  DatePicker,
+  Dropdown,
   SelectControl, 
   TextControl,
   __experimentalText as Text,
@@ -20,6 +23,9 @@ const Edit = () => {
   const isExhibit = SelectUtils.editedPostAttribute('type') === 'exhibit';
   if ( !isExhibit )  return html`<${Fragment} />`;
 
+  // get various select options
+  const curatorOrgs = SelectUtils.terms('curator', {per_page: '-1', orderby: 'name', order: 'asc'});
+
   const {editEntityRecord} = useDispatch( 'core' );
 
   const [ modalIsOpen, setModalOpen ] = useState( false );
@@ -30,31 +36,49 @@ const Edit = () => {
     setModalOpen( true )
   };
 	const closeModal = () => setModalOpen( false );
-  
-  const saveMetadata = () => {
-    if ( topExhibit ) {
-      const data = {
-        title: exhibitTitle,
-        meta: {
-          isOnline: exhibitIsOnline
-        }
-      }
-      editEntityRecord('postType', 'exhibit', topExhibit, data);
-    } else {
-
-    }
-    closeModal();
-  }
 
   // set default metadata to current page
   const postTitle = SelectUtils.editedPostAttribute('title');
-  const meta = SelectUtils.editedPostAttribute('meta');
+  const postMeta = SelectUtils.editedPostAttribute('meta');
+  const postCuratorOrgs = SelectUtils.editedPostAttribute('curator');
   const watchedVars = [
-    meta.isOnline
+    postTitle,
+    postMeta.isOnline,
+    postMeta.isPhysical,
+    postMeta.isPermanent,
+    postMeta.dateFrom,
+    postMeta.dateTo,
+    postCuratorOrgs
   ];
   const { editPost } = useDispatch( 'core/editor', watchedVars );
   const [ exhibitTitle, setExhibitTitle ] = useState( postTitle );
-  const [ exhibitIsOnline, setExhibitIsOnline ] = useState( meta.isOnline );
+  const [ exhibitIsOnline, setExhibitIsOnline ] = useState( postMeta.isOnline );
+  const [ exhibitIsPhysical, setExhibitIsPhysical ] = useState( postMeta.isPhysical );
+  const [ exhibitIsPermanent, setExhibitIsPermanent ] = useState( postMeta.isPermanent );
+  const [ exhibitDateFrom, setExhibitDateFrom ] = useState( postMeta.dateFrom );
+  const [ exhibitDateTo, setExhibitDateTo ] = useState( postMeta.dateTo );
+  const [ exhibitCuratorOrgs, setExhibitCuratorOrgs] = useState(postCuratorOrgs);
+
+  // write metadata to current page or top-level parent
+  const saveMetadata = () => {
+    const data = {
+      title: exhibitTitle,
+      curator: exhibitCuratorOrgs,
+      meta: {
+        isOnline: exhibitIsOnline,
+        isPhysical: exhibitIsPhysical,
+        isPermanent: exhibitIsPhysical ? exhibitIsPermanent : false,
+        dateFrom: (exhibitIsPhysical && !exhibitIsPermanent) ? exhibitDateFrom : null,
+        dateTo: (exhibitIsPhysical && !exhibitIsPermanent) ? exhibitDateTo : null,
+      }
+    }
+    if ( topExhibit ) {
+      editEntityRecord('postType', 'exhibit', topExhibit, data);
+    } else {
+      editPost(data);
+    }
+    closeModal();
+  }
 
 
   // retrieve metadata from parent if applicable
@@ -74,12 +98,44 @@ const Edit = () => {
         setTopExhibit(r.exhibitId);
         setExhibitTitle(r.exhibitTitle);
         setExhibitIsOnline(r.exhibitIsOnline);
+        setExhibitIsPhysical(r.exhibitIsPhysical);
+        setExhibitDateFrom(r.exhibitDateFrom);
+        setExhibitDateTo(r.exhibitDateTo);
+        setExhibitCuratorOrgs(r.exhibitCuratorOrgs.map(org => org.term_id));
       }, 
       (error) => {
         setParentError(true);
       })
 
   }, [parent]);
+
+  const datePickerDropdown = (onClose, field) => {
+    const setter = field == 'from' ? setExhibitDateFrom : setExhibitDateTo;
+    const value = field == 'from' ? exhibitDateFrom : exhibitDateTo;
+    const onChange = (v) => {
+      setter(v.split('T')[0].replace(/-/g, ''));
+      onClose();
+    }
+    const onReset = () => {
+      setter(null);
+      onClose(); 
+    }
+    return html`
+      <div>
+        <${DatePicker} currentDate=${value} onChange=${onChange} />
+        ${value && html`
+          <${Button} variant='link' isDestructive=${true} onClick=${onReset}>Reset</${Button}>
+        `}
+      </div>
+    `
+  }
+
+  const toggleArrayMember = (needle, haystack) => {
+    if ( haystack.includes(needle) ) {
+      return haystack.filter(v => v != needle );
+    }
+    return [...haystack, needle];
+  }
 
   return html`
 
@@ -95,22 +151,81 @@ const Edit = () => {
                 <div><p>There was an error when retrieving exhibit metadata. Please try again later.</p></div>
               ` : html`
                 <div>
-                  <${TextControl} 
-                    label='Exhibit Title'
-                    value=${exhibitTitle}
-                    onChange=${(v) => setExhibitTitle(v)}
-                  />
+                  <div style=${{marginBottom: '15px'}}>
+                    <${TextControl} 
+                      label='Exhibit Title'
+                      value=${exhibitTitle}
+                      onChange=${(v) => setExhibitTitle(v)}
+                    />
+                  </div>
                   <${ToggleControl} 
-                    label='Is Online Exhibit'
+                    label='Exhibit has an Online Component'
                     checked=${exhibitIsOnline}
                     onChange=${() => {setExhibitIsOnline(!exhibitIsOnline)}}
                   />
+                  <${ToggleControl} 
+                    label='Exhibit has/had a Physical Installation'
+                    checked=${exhibitIsPhysical}
+                    onChange=${() => {setExhibitIsPhysical(!exhibitIsPhysical)}}
+                  />
+                  ${ exhibitIsPhysical && html`
+                    <${ToggleControl} 
+                      label='Exhibit is a Permanent Installation'
+                      checked=${exhibitIsPermanent}
+                      onChange=${() => {setExhibitIsPermanent(!exhibitIsPermanent)}}
+                    />
+                  `}
+                  ${(exhibitIsPhysical && !exhibitIsPermanent) && html`
+                    <div>
+                      <h3>Dates</h3>
+                      <div style=${{margin: '10px 0'}}>
+                        <${Dropdown} 
+                          renderToggle=${({onToggle }) => html`
+                            <div onClick=${onToggle} style=${{cursor:'pointer'}}> 
+                              <span>Start: </span>
+                              <span>${exhibitDateFrom || 'Not Set'}</span>
+                            </div>
+                          `}
+                          renderContent=${({ onClose }) => datePickerDropdown(onClose, 'from')}
+                        />
+                      </div>
+                      <div style=${{margin: '10px 0'}}>
+                        <${Dropdown} 
+                          renderToggle=${({onToggle }) => html`
+                            <div onClick=${onToggle} style=${{cursor:'pointer'}}> 
+                              <span>End: </span>
+                              <span>${exhibitDateTo || 'Not Set'}</span>
+                            </div>
+                          `}
+                          renderContent=${({ onClose }) => datePickerDropdown(onClose, 'to')}
+                        />
+                      </div>
+                    </div>
+                  `}
+
+                  <div>
+                    <h3>Curators</h3>
+                    <div style=${{marginBottom: '10px'}}>
+                      <h4>Curating Organizations:</h4>
+                      ${curatorOrgs.map(org => html`
+                        <div key=${org.id}>
+                          <${CheckboxControl} 
+                            label=${org.name}
+                            checked=${exhibitCuratorOrgs.includes(org.id)}
+                            onChange=${ () => setExhibitCuratorOrgs(toggleArrayMember(org.id, exhibitCuratorOrgs))}
+                          />
+                        </div>
+                      `)}
+                    </div>
+                  </div>
+
+
                   <div style=${{margin: '10px 0'}}>
-                    <${Button} onClick=${saveMetadata} variant="primary">${topExhibit ? 'Queue Changes' : 'Save'}</${Button}>
+                    <${Button} onClick=${saveMetadata} variant="primary">Save</${Button}>
                     <${Button} onClick=${closeModal} variant="secondary">Close</${Button}>
                   </div>
                   ${topExhibit != 0 && html`
-                    <${Text} isBlock variant='muted'>After queueing a change, you must still 'Update' this page to save.</${Text}>
+                    <${Text} isBlock variant='muted'>After saving changes, you must still 'Update' this page for your changes to take effect.</${Text}>
                   `}
                 </div>
               `}
