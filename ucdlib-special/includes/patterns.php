71889cacc3336cc066e19dcd257d6e7ba95caf8d
UcDust <dcartwright@ucdavis.edu>
2022-07-01 10:26:15 -0700

    remove sidebar from pattern since itll be a widget

--- ucdlib-special/includes/patterns.php
+++ ucdlib-special/includes/patterns.php
@@ -39,24 +39,6 @@ class UCDLibPluginSpecialPatterns {
       <!-- wp:ucd-theme/special-subject /-->
       <!-- /wp:ucd-theme/column -->
       <!-- wp:ucd-theme/column {\"layoutClass\":\"l-sidebar-first\",\"forbidWidthEdit\":true} -->
-      <!-- wp:ucd-theme/background-color -->
-      <!-- wp:paragraph -->
-      <p><strong>Many collections are stored offsite and should be requested at least 7 business days in advance of your visit.</strong></p>
-      <!-- /wp:paragraph -->
-      <!-- /wp:ucd-theme/background-color -->
-      <!-- wp:heading {\"level\":4} -->
-      <h4 id=\"how-to-access-materials\">How to Access Materials</h4>
-      <!-- /wp:heading -->
-      <!-- wp:paragraph -->
-      <p>To request an item, go to the <strong>Finding Aid</strong> linked on the item's information page and select the \"Request Items\" button. This will initiate a request form for that item within the Aeon request system.</p>
-      <!-- /wp:paragraph -->
-      <!-- wp:paragraph -->
-      <p>Please check your Aeon account to confirm that the item is listed as \"Available in Reading Room\" before visiting. </p>
-      <!-- /wp:paragraph -->
-      <!-- wp:ucd-theme/button-link {\"content\":\"Login to Aeon\"} /-->
-      <!-- wp:paragraph -->
-      <p><strong><em>New to Aeon?</em></strong> <a href=\"http://library.ucdavis.edu\">Create an account</a></p>
-      <!-- /wp:paragraph -->
       <!-- /wp:ucd-theme/column -->
       <!-- /wp:ucd-theme/layout-basic -->
     ";
