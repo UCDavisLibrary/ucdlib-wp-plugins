68e0a434e1038535be3bce5723b443c3e7092fe0
spelkey-ucd <spelkey@ucdavis.edu>
2022-08-19 16:09:18 -0400

    add excerpt option to exhibit query block. #112

--- ucdlib-assets/ucdlib-assets.php
+++ ucdlib-assets/ucdlib-assets.php
@@ -3,7 +3,7 @@
  * Plugin Name: UCD Library Assets
  * Plugin URI: https://github.com/UCDavisLibrary/ucdlib-wp-plugins/tree/main/ucdlib-assets
  * Description: Manages UCD Library static assets. Primarily, builds and loads JS bundles from theme and plugins.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: UC Davis Library Online Strategy
  */
 
