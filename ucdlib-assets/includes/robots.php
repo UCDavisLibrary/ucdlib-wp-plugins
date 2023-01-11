312fa3e6ba6ec0675c0d0f7e4855e2e1e075dcc5
spelkey-ucd <spelkey@ucdavis.edu>
2023-01-11 16:41:57 -0500

    #143

--- ucdlib-assets/includes/robots.php
+++ ucdlib-assets/includes/robots.php
@@ -3,7 +3,15 @@
 class UCDLibPluginAssetsRobots {
 
   public function __construct(){
+    $this->site_url = get_site_url();
+    $this->prodUrl = 'https://library.ucdavis.edu';
+    $this->isProd =  $this->site_url == $this->prodUrl;
+
     add_filter('robots_txt', [$this, 'interceptRobotsTxt'], 10, 2);
+    add_filter( 'wp_robots', [$this, 'robotsMeta'], 100 );
+
+    // remove default wordpress behavior
+    remove_filter('wp_robots', 'wp_robots_max_image_preview_large');
   }
 
   /**
@@ -13,16 +21,34 @@ class UCDLibPluginAssetsRobots {
    */
   public function interceptRobotsTxt( $output, $public ) {
 
-    $site_url = get_site_url();
-    if ( $site_url == 'https://library.ucdavis.edu') {
+    // TODO: remove this part after google has reindex our dev sites
+    $output = "User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php\n\n";
+    $output .= "Sitemap: $this->site_url/wp-sitemap.xml";
+    return $output;
+
+    if ( $this->isProd ) {
       $output = "User-agent: *\nDisallow: /wp-admin/\nAllow: /wp-admin/admin-ajax.php\n\n";
-      $output .= "Sitemap: https://library.ucdavis.edu/wp-sitemap.xml";
+      $output .= "Sitemap: $this->prodUrl/wp-sitemap.xml";
     } else {
       $output = "User-agent: *\nDisallow: /\n";
     }
-    
 
     return $output;
+  }
+
+  /**
+   * Generates the <meta name='robots'> tag
+   */
+  public function robotsMeta($robots){
+    if ( $this->isProd ){
+      $robots = ['all' => true];
+    } else {
+      $robots = [
+        'noindex' => true,
+        'nofollow' => true
+      ];
+    }
+    return $robots;
   }
 
   
