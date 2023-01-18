669ab751b724f4ee8e00f0a8765207ec7a1d095d
spelkey-ucd <spelkey@ucdavis.edu>
2023-01-18 16:13:18 -0500

    #146

--- ucdlib-directory/includes/people.php
+++ ucdlib-directory/includes/people.php
@@ -376,7 +376,21 @@ class UCDLibPluginDirectoryPeople {
     }
 
     $context = Timber::context();
+    $title = "Page Not Found";
+    $context['title'] = $title;
     status_header(404);
+
+    $widgetArea =  Timber::get_widgets( 'four-oh-four' );
+    if ( trim($widgetArea) ) {
+      $context['content'] = $widgetArea;
+    } else {
+      $context['content'] = "<p>Sorry, we couldn't find what you're looking for.</p>";
+    }
+
+    $context['breadcrumbs'] = [
+      ['link' => '/', 'title' => 'Home'],
+      ['link' => "", 'title' => $title]
+    ];
     $views = $GLOBALS['UcdSite']->views;
     $templates = array( $views->getTemplate('404'));
     Timber::render( $templates, $context );
